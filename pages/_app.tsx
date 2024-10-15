85eac04e5be3c3cc47b92337e6603aff99e22eae
yamamoto2281 <yamamotosnm@nttdata-ccs.co.jp>
2024-10-15 14:46:35 +0900

    202410151446

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,68 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { supabase } from '../utils/supabase'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
-
+  switch (metric.name) {
+    case 'FCP':
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'LCP':
+      console.log(`LCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'TTFB':
+      console.log(`TTFB: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'Next.js-hydration':
+      console.log(
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${
+          Math.round((metric.startTime + metric.value) * 10) / 10
+        }`
+      )
+      break
+    default:
+      break
+  }
 }
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/dashboard')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event: string, _: any) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
